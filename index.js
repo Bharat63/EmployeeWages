422f7963c4b9eaf62ccabe9cf4729c72e8384d53
Bharat sharma <bksharma1881@gmail.com>
2025-03-20 15:40:10 +0530

    Stored daily wage in an array

--- index.js
+++ index.js
@@ -1,72 +1,94 @@
-//Employee Presence(UC1)
+// UC1 (Employee Presence)
 const IS_ABSENT = 0;
-let empCheck = Math.floor(Math.random() * 10) % 2;
-if (empCheck == IS_ABSENT) {
-  console.log("Employee is Absent");
-  return;
-} else {
-  console.log("Employee is Present");
-}
 
-//Employee Wages UC2
+let empCheck = Math.floor(Math.random()*10)%3;
+if (empCheck == IS_ABSENT) {
+    console.log("Employee is Absent");
+    return;
+}
+else{
+    console.log("Employee is Present");
+}
+// console.log(empCheck);
+
+
+// UC2 (Employee Wage)
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
-// let empHrs =0;
-// empCheck=Math.floor(Math.random()*10)%3;
-// switch (empCheck){
+
+// let empHrs = 0;
+// let empCheck = Math.floor(Math.random()*10)%3;
+// console.log(empCheck);
+// switch(empCheck){
 //     case IS_PART_TIME:
-//         empHrs=PART_TIME_HOURS;
+//         empHrs = PART_TIME_HOURS;
 //         break;
 //     case IS_FULL_TIME:
-//         empHrs=FULL_TIME_HOURS;
+//         empHrs = FULL_TIME_HOURS;
 //         break;
 //     default:
-//         empHrs=0;
+//         empHrs = 0;
 // }
-// let empWage=empHrs*WAGE_PER_HOUR;
-// console.log("Emp Wage "+ empWage);
 
-//UC3- Get working hours
-function getWorkingHours(empCheck) {
-  switch (empCheck) {
-    case IS_PART_TIME:
-      return PART_TIME_HOURS;
-    case IS_FULL_TIME:
-      return FULL_TIME_HOURS;
-    default:
-      return 0;
-  }
+// let empWage = empHrs*WAGE_PER_HOUR;
+// console.log("Emp Wage: " + empWage);
+
+// UC3 (getWorkingHours function added)
+function getWorkingHours(empCheck){
+    switch(empCheck){
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;    
+        default:
+            return 0;
+    }
 }
-// let empHrs = 0;
-// empCheck = Math.floor(Math.random() * 10) % 3;
-// empHrs = getWorkingHours(empCheck);
-// let empWage = empHrs * WAGE_PER_HOUR;
-// console.log("Emp Wage : " + empWage);
 
-// UC 4
+// let empHrs = 0;
+// let empCheck = Math.floor(Math.random()*10)%3;
+// empHrs = getWorkingHours(empCheck);
+// let empWage = empHrs*WAGE_PER_HOUR;
+// console.log("Emp Wage: " + empWage);
+
+// UC4 (Calculating Wages for a month(20 days))
 const NUM_OF_WORKING_DAYS = 20;
 let empHrs = 0;
-for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
-  let empCheck = Math.floor(Math.random() * 10) % 3;
-  empHrs += getWorkingHours(empCheck);
+
+for(let day = 0; day < NUM_OF_WORKING_DAYS; day++){
+    let empCheck = Math.floor(Math.random()*10)%3;
+    empHrs += getWorkingHours(empCheck);
 }
-// let empWage = empHrs * WAGE_PER_HOUR;
+// let empWage = empHrs*WAGE_PER_HOUR;
 // console.log("Total Hrs: " + empHrs + " Emp Wage: " + empWage);
 
-
-
-// UC 5
+// UC5 (max hours and max days implemented)
 const MAX_HRS_IN_MONTH = 160;
-// const NUM_OF_WORKING_DAYS = 10;
 let totalEmpHrs = 0;
 let totalWorkingDays = 0;
-while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
-totalWorkingDays++;
-let empCheck = Math.floor(Math.random() * 10) % 3;
-totalEmpHrs += getWorkingHours(empCheck);
+// while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS){
+//     totalWorkingDays++;
+//     let empCheck = Math.floor(Math.random()*10)%3;
+//     totalEmpHrs += getWorkingHours(empCheck);
+// }
+
+// let empWage = empHrs*WAGE_PER_HOUR;
+// console.log("UC5 - Total Days: " + totalWorkingDays +" Total Hrs: " + totalEmpHrs + " Emp Wage: " + empWage);
+
+// UC6 (Save daily wage in an array)
+function calcDailyWage(empHrs) {
+    return empHrs*WAGE_PER_HOUR;
 }
-let empWage = totalEmpHrs * WAGE_PER_HOUR;
-console.log("UC5 - Total Days: " + totalWorkingDays + " Total Hrs: " + totalEmpHrs + " Emp Wage: " + empWage);
+let empDailyWageArr = new Array();
+while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS){
+    totalWorkingDays++;
+    let empCheck = Math.floor(Math.random()*10)%3;
+    let empHrs = getWorkingHours(empCheck);
+    totalEmpHrs += empHrs;
+    empDailyWageArr.push(calcDailyWage(empHrs));
+}
+let empWage = calcDailyWage(totalEmpHrs);
+console.log("UC6 - Total Days: " + totalWorkingDays +" Total Hrs: " + totalEmpHrs + " Emp Wage: " + empWage);
